6c546ef4b65f2ad2e9e6fc22928665d9e33a7616
gavboler03 <gavin.boler@students.fhu.edu>
2023-11-26 20:19:57 -0600

    Updated frontend.

--- fakeTrueDatasetModel.py
+++ fakeTrueDatasetModel.py
@@ -1,54 +1,64 @@
 import numpy as np
 import pandas as pd
-import itertools
-import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+import streamlit as st
 
 
-def Model():
+def Model(input_data):
     # Load Data
     fake = pd.read_csv("Fake.csv")
     trueNew = pd.read_csv("True.csv")
+    fake.columns
 
-    # Make a new column for fake and true datasets
+    # Create New Column For New DataFrame
     fake["Truth"] = 0
     trueNew["Truth"] = 1
 
-    # Combine fake and true datasets
+    # Combine Fake and True DataSets
     news = pd.concat([fake, trueNew])
-    news.head()
 
-    # Clean data
+    # Clean Data
     news.dropna()
 
-    # Get labels
-    truth = news.Truth
-    truth.head()
-    news.columns
-
-    # Split dataset
+    # Split Data Into Test and Training sets
     X_train, X_test, Y_train, Y_test = train_test_split(
-        news["story"], truth, test_size=0.2, random_state=7
+        news["story"], news["Truth"], test_size=0.2, random_state=7
     )
 
-    # Use the TfidfVectorizer
+    # Set the TfidfVectorizer
     tfidf_vectorizer = TfidfVectorizer(stop_words="english", max_df=0.7)
-
     tfidf_train = tfidf_vectorizer.fit_transform(X_train)
     tfidf_test = tfidf_vectorizer.transform(X_test)
 
-    # Initialize PassiveAggresive Classifier
-    pac = PassiveAggressiveClassifier(max_iter=50)
+    # Train the PassiveAgressive Classifier
+    pac = PassiveAggressiveClassifier(max_iter=100)
     pac.fit(tfidf_train, Y_train)
 
     # Set Accuracy
     y_pred = pac.predict(tfidf_test)
     score = accuracy_score(Y_test, y_pred)
-    acc = f"Accuracy: {round(score*100,2)}%"
+    print(f"Accuracy: {round(score*100,2)}%")
 
-    # Confusion Matrix
+    # Set Confusion Matrix
     confusion_matrix(Y_test, y_pred, labels=[0, 1])
-    return acc
+    if y_pred[1] == 0:
+        return "This is fake news."
+    else:
+        return "This is real news."
+
+
+def main():
+    st.title("Fake News Detector")
+    news_input = st.text_input("Paste news article here.")
+    answer = ""
+
+    if st.button("Submit"):
+        answer = Model(news_input)
+
+    st.success(answer)
+
+
+main()
